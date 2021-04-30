1c42fee1732b7451acd0f3da50b7a7f8b8bc8cb3
Adrian Hesketh <adrianhesketh@hushmail.com>
2021-04-30 19:03:04 +0100

    got the LSP proxy working

--- cmd/lsp/handler.go
+++ cmd/lsp/handler.go
@@ -82,6 +82,25 @@ func (h Handler) internal(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc
 		}
 		return nil, nil
 
+	case "textDocument/didOpen":
+		if req.Params == nil {
+			return nil, &jsonrpc2.Error{Code: jsonrpc2.CodeInvalidParams}
+		}
+		var params lsp.DidOpenTextDocumentParams
+		if err := json.Unmarshal(*req.Params, &params); err != nil {
+			return nil, err
+		}
+
+	case "textDocument/didClose":
+		if req.Params == nil {
+			return nil, &jsonrpc2.Error{Code: jsonrpc2.CodeInvalidParams}
+		}
+		var params lsp.DidCloseTextDocumentParams
+		if err := json.Unmarshal(*req.Params, &params); err != nil {
+			return nil, err
+		}
+		return h.handleDidClose(ctx, conn, req, params)
+
 	case "textDocument/hover":
 		if req.Params == nil {
 			return nil, &jsonrpc2.Error{Code: jsonrpc2.CodeInvalidParams}
@@ -90,30 +109,7 @@ func (h Handler) internal(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc
 		if err := json.Unmarshal(*req.Params, &params); err != nil {
 			return nil, err
 		}
-		//return h.handleHover(ctx, conn, req, params)
-		return nil, nil
-
-	case "textDocument/definition":
-		if req.Params == nil {
-			return nil, &jsonrpc2.Error{Code: jsonrpc2.CodeInvalidParams}
-		}
-		var params lsp.TextDocumentPositionParams
-		if err := json.Unmarshal(*req.Params, &params); err != nil {
-			return nil, err
-		}
-		//return h.handleDefinition(ctx, conn, req, params)
-		return nil, nil
-
-	case "textDocument/typeDefinition":
-		if req.Params == nil {
-			return nil, &jsonrpc2.Error{Code: jsonrpc2.CodeInvalidParams}
-		}
-		var params lsp.TextDocumentPositionParams
-		if err := json.Unmarshal(*req.Params, &params); err != nil {
-			return nil, err
-		}
-		//return h.handleTypeDefinition(ctx, conn, req, params)
-		return nil, nil
+		return h.handleTextDocumentHover(ctx, conn, req, params)
 
 	case "textDocument/completion":
 		if req.Params == nil {
@@ -125,17 +121,6 @@ func (h Handler) internal(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc
 		}
 		return h.handleTextDocumentCompletion(ctx, conn, req, params)
 
-	case "textDocument/implementation":
-		if req.Params == nil {
-			return nil, &jsonrpc2.Error{Code: jsonrpc2.CodeInvalidParams}
-		}
-		var params lsp.TextDocumentPositionParams
-		if err := json.Unmarshal(*req.Params, &params); err != nil {
-			return nil, err
-		}
-		//return h.handleTextDocumentImplementation(ctx, conn, req, params)
-		return nil, nil
-
 	case "textDocument/signatureHelp":
 		if req.Params == nil {
 			return nil, &jsonrpc2.Error{Code: jsonrpc2.CodeInvalidParams}
@@ -144,10 +129,57 @@ func (h Handler) internal(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc
 		if err := json.Unmarshal(*req.Params, &params); err != nil {
 			return nil, err
 		}
-		//return h.handleTextDocumentSignatureHelp(ctx, conn, req, params)
-		return nil, nil
+		return h.handleTextDocumentSignatureHelp(ctx, conn, req, params)
 	}
 	return nil, &jsonrpc2.Error{Code: jsonrpc2.CodeMethodNotFound, Message: fmt.Sprintf("method not supported: %s", req.Method)}
+}
+
+func (h Handler) handleDidOpen(ctx context.Context, conn jsonrpc2.JSONRPC2, req *jsonrpc2.Request, params lsp.DidOpenTextDocumentParams) (*lsp.TextDocumentSyncOptions, error) {
+	// Get the client to always send the whole document.
+	return &lsp.TextDocumentSyncOptions{
+		OpenClose: true,
+		Change:    lsp.TDSKFull,
+	}, nil
+}
+
+func (h Handler) handleDidClose(ctx context.Context, conn jsonrpc2.JSONRPC2, req *jsonrpc2.Request, params lsp.DidCloseTextDocumentParams) (*lsp.TextDocumentSyncOptions, error) {
+	// Get the client to always send the whole document.
+	return &lsp.TextDocumentSyncOptions{
+		OpenClose: true,
+		Change:    lsp.TDSKFull,
+	}, nil
+}
+
+func (h Handler) handleTextDocumentSignatureHelp(ctx context.Context, conn jsonrpc2.JSONRPC2, req *jsonrpc2.Request, params lsp.TextDocumentPositionParams) (*lsp.SignatureHelp, error) {
+	return &lsp.SignatureHelp{
+		Signatures: []lsp.SignatureInformation{
+			{
+				Label:         "signature",
+				Documentation: "doc",
+				Parameters: []lsp.ParameterInformation{
+					{
+						Label:         "param1",
+						Documentation: "param1Doc",
+					},
+				},
+			},
+		},
+	}, nil
+}
+
+func (h Handler) handleTextDocumentHover(ctx context.Context, conn jsonrpc2.JSONRPC2, req *jsonrpc2.Request, params lsp.TextDocumentPositionParams) (*lsp.Hover, error) {
+	return &lsp.Hover{
+		Contents: []lsp.MarkedString{
+			{
+				Language: "go",
+				Value:    "Hover handler",
+			},
+		},
+		Range: &lsp.Range{
+			Start: lsp.Position{Line: params.Position.Line, Character: params.Position.Character},
+			End:   lsp.Position{Line: params.Position.Line, Character: params.Position.Character},
+		},
+	}, nil
 }
 
 func (h Handler) handleTextDocumentCompletion(ctx context.Context, conn jsonrpc2.JSONRPC2, req *jsonrpc2.Request, params lsp.CompletionParams) (*lsp.CompletionList, error) {
--- cmd/lsp/proxy/main.go
+++ cmd/lsp/proxy/main.go
@@ -25,6 +25,12 @@ var rpcInitialize = RPC{
 	Payload: `{"processId":71156,"rootPath":"/Users/adrian/github.com/a-h/templ","rootUri":"file:///Users/adrian/github.com/a-h/templ","capabilities":{"workspace":{"applyEdit":true,"workspaceEdit":{"documentChanges":true,"resourceOperations":["create","rename","delete"],"failureHandling":"textOnlyTransactional"},"didChangeConfiguration":{"dynamicRegistration":true},"didChangeWatchedFiles":{"dynamicRegistration":true},"symbol":{"dynamicRegistration":true,"symbolKind":{"valueSet":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]},"tagSupport":{"valueSet":[1]}},"executeCommand":{"dynamicRegistration":true},"configuration":true,"workspaceFolders":true},"textDocument":{"publishDiagnostics":{"relatedInformation":true,"versionSupport":false,"tagSupport":{"valueSet":[1,2]}},"synchronization":{"dynamicRegistration":true,"willSave":true,"willSaveWaitUntil":true,"didSave":true},"completion":{"dynamicRegistration":true,"contextSupport":true,"completionItem":{"snippetSupport":true,"commitCharactersSupport":true,"documentationFormat":["markdown","plaintext"],"deprecatedSupport":true,"preselectSupport":true,"tagSupport":{"valueSet":[1]}},"completionItemKind":{"valueSet":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]}},"hover":{"dynamicRegistration":true,"contentFormat":["markdown","plaintext"]},"signatureHelp":{"dynamicRegistration":true,"contextSupport":true,"signatureInformation":{"documentationFormat":["markdown","plaintext"],"activeParameterSupport":true,"parameterInformation":{"labelOffsetSupport":true}}},"definition":{"dynamicRegistration":true},"references":{"dynamicRegistration":true},"documentHighlight":{"dynamicRegistration":true},"documentSymbol":{"dynamicRegistration":true,"symbolKind":{"valueSet":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]},"hierarchicalDocumentSymbolSupport":true,"tagSupport":{"valueSet":[1]}},"codeAction":{"dynamicRegistration":true,"isPreferredSupport":true,"codeActionLiteralSupport":{"codeActionKind":{"valueSet":["","quickfix","refactor","refactor.extract","refactor.inline","refactor.rewrite","source","source.organizeImports"]}}},"codeLens":{"dynamicRegistration":true},"formatting":{"dynamicRegistration":true},"rangeFormatting":{"dynamicRegistration":true},"onTypeFormatting":{"dynamicRegistration":true},"rename":{"dynamicRegistration":true,"prepareSupport":true},"documentLink":{"dynamicRegistration":true,"tooltipSupport":true},"typeDefinition":{"dynamicRegistration":true},"implementation":{"dynamicRegistration":true},"declaration":{"dynamicRegistration":true},"colorProvider":{"dynamicRegistration":true},"foldingRange":{"dynamicRegistration":true,"rangeLimit":5000,"lineFoldingOnly":true},"selectionRange":{"dynamicRegistration":true}},"window":{"workDoneProgress":true}},"initializationOptions":{},"trace":"off","workspaceFolders":[{"uri":"file:///Users/adrian/github.com/a-h/templ","name":"templ"}],"clientInfo":{"name":"coc.nvim","version":"0.0.80"},"workDoneToken":"3ae4d8b2-f46a-46cc-886e-ab2ebafaa5e2"}`,
 }
 
+var rpcInitialized = RPC{
+	Method:  "initialized",
+	Payload: `{}`,
+	Notify:  true,
+}
+
 var rpcDidOpen = RPC{
 	Method:  "textDocument/didOpen",
 	Payload: `{"textDocument":{"uri":"file:///Users/adrian/github.com/a-h/templ/cmd/example/address.go","languageId":"go","version":1,"text":"package templ\n\ntype Address struct {\n\tAddress1 string\n\tAddress2 string\n\tAddress3 string\n\tAddress4 string\n}\n\ntype Person struct {\n\t// Addresses of the person.\n\tAddresses []Address\n\t// Type of person.\n\tType string\n\t// URL of person.\n\tURL string\n}\n\n// ID of the person.\nfunc (p Person) ID() string {\n\treturn \"123\"\n}\n\n// Name of the person.\nfunc (p Person) Name() string {\n\treturn \"Person Name\"\n}\n\nfunc init() {\n\tp := Person{}\n\tp.ID()\n}\n"}}`,
@@ -38,6 +44,7 @@ var rpcCompletion = RPC{
 
 var payloads []RPC = []RPC{
 	rpcInitialize,
+	rpcInitialized,
 	rpcDidOpen,
 	rpcCompletion,
 }
@@ -45,10 +52,17 @@ var payloads []RPC = []RPC{
 type RPCHandler struct{}
 
 func (h RPCHandler) Handle(ctx context.Context, conn *jsonrpc2.Conn, r *jsonrpc2.Request) {
-	fmt.Println("<-- Server method:", r.Method)
-	jr, _ := r.Params.MarshalJSON()
-	fmt.Println("<-- Server reponse:", string(jr))
-	conn.Reply(ctx, r.ID, struct{}{})
+	fmt.Println("<-- From server method:", r.Method)
+	jr, err := r.Params.MarshalJSON()
+	if err != nil {
+		fmt.Println("RPCHandler.Handle: failed to marshal JSON", err)
+	}
+	fmt.Println("<-- From server request:", string(jr))
+	// Return nil by default.
+	err = conn.Reply(ctx, r.ID, nil)
+	if err != nil {
+		fmt.Println("RPCHandler.Handle: reply failed:", err)
+	}
 }
 
 func runRPC() (s string, err error) {
@@ -82,7 +96,7 @@ func runRPC() (s string, err error) {
 		err = conn.Call(context.Background(), pl.Method, &input, &output)
 		if err != nil {
 			err = fmt.Errorf("error making %q call: %w", pl.Method, err)
-			return
+			continue
 		}
 		jd, err := json.MarshalIndent(output, "", " ")
 		if err != nil {
