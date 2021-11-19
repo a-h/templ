e6023b14f5dafa5e2e342f8664655c4292d9b665
Adrian Hesketh <adrianhesketh@hushmail.com>
2021-11-19 11:27:50 +0000

    feat: add turbo stream support.

---
+++ turbo/stream.go
@@ -0,0 +1,70 @@
+package turbo
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/a-h/templ"
+)
+
+// Append adds an append action to the output stream.
+func Append(w http.ResponseWriter, target string, template templ.Component) error {
+	return AppendWithContext(context.Background(), w, target, template)
+}
+
+// AppendWithContext adds an append action to the output stream.
+func AppendWithContext(ctx context.Context, w http.ResponseWriter, target string, template templ.Component) error {
+	w.Header().Set("Content-Type", "text/vnd.turbo-stream.html")
+	return actionTemplate("append", target, template).Render(ctx, w)
+}
+
+// Prepend adds a prepend action to the output stream.
+func Prepend(w http.ResponseWriter, target string, template templ.Component) error {
+	return PrependWithContext(context.Background(), w, target, template)
+}
+
+// PrependWithContext adds a prepend action to the output stream.
+func PrependWithContext(ctx context.Context, w http.ResponseWriter, target string, template templ.Component) error {
+	w.Header().Set("Content-Type", "text/vnd.turbo-stream.html")
+	return actionTemplate("prepend", target, template).Render(ctx, w)
+}
+
+// Replace adds a replace action to the output stream.
+func Replace(w http.ResponseWriter, target string, template templ.Component) error {
+	return ReplaceWithContext(context.Background(), w, target, template)
+}
+
+// ReplaceWithContext adds a replace action to the output stream.
+func ReplaceWithContext(ctx context.Context, w http.ResponseWriter, target string, template templ.Component) error {
+	w.Header().Set("Content-Type", "text/vnd.turbo-stream.html")
+	return actionTemplate("replace", target, template).Render(ctx, w)
+}
+
+// Update adds an update action to the output stream.
+func Update(w http.ResponseWriter, target string, template templ.Component) error {
+	return UpdateWithContext(context.Background(), w, target, template)
+}
+
+// UpdateWithContext adds an update action to the output stream.
+func UpdateWithContext(ctx context.Context, w http.ResponseWriter, target string, template templ.Component) error {
+	w.Header().Set("Content-Type", "text/vnd.turbo-stream.html")
+	return actionTemplate("update", target, template).Render(ctx, w)
+}
+
+// Remove adds a remove action to the output stream.
+func Remove(w http.ResponseWriter, target string) error {
+	return RemoveWithContext(context.Background(), w, target)
+}
+
+// RemoveWithContext adds a remove action to the output stream.
+func RemoveWithContext(ctx context.Context, w http.ResponseWriter, target string) error {
+	w.Header().Set("Content-Type", "text/vnd.turbo-stream.html")
+	return removeTemplate("remove", target).Render(ctx, w)
+}
+
+// IsTurboRequest returns true if the incoming request is able to receive a Turbo stream.
+// This is determined by checking the request header for "text/vnd.turbo-stream.html"
+func IsTurboRequest(r *http.Request) bool {
+	return strings.Contains(r.Header.Get("accept"), "text/vnd.turbo-stream.html")
+}
---
+++ turbo/stream_test.go
@@ -0,0 +1,92 @@
+package turbo
+
+import (
+	"context"
+	"io"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/a-h/templ"
+)
+
+var contentTemplate = templ.ComponentFunc(func(ctx context.Context, w io.Writer) error {
+	_, err := io.WriteString(w, "content")
+	return err
+})
+
+func TestStream(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	// Append.
+	expectedAppends := 2
+	for i := 0; i < expectedAppends; i++ {
+		if err := Append(w, "appendTarget", contentTemplate); err != nil {
+			t.Fatalf("append failed: %v", err)
+		}
+	}
+	// Prepend.
+	expectedPrepends := 3
+	for i := 0; i < expectedPrepends; i++ {
+		if err := Prepend(w, "prependTarget", contentTemplate); err != nil {
+			t.Fatalf("prepend failed: %v", err)
+		}
+	}
+	// Replace.
+	expectedReplaces := 4
+	for i := 0; i < expectedReplaces; i++ {
+		if err := Replace(w, "replaceTarget", contentTemplate); err != nil {
+			t.Fatalf("replace failed: %v", err)
+		}
+	}
+	// Update.
+	expectedUpdates := 5
+	for i := 0; i < expectedUpdates; i++ {
+		if err := Update(w, "updateTarget", contentTemplate); err != nil {
+			t.Fatalf("update failed: %v", err)
+		}
+	}
+	// Remove.
+	expectedRemoves := 6
+	for i := 0; i < expectedRemoves; i++ {
+		if err := Remove(w, "removeTarget"); err != nil {
+			t.Fatalf("remove failed: %v", err)
+		}
+	}
+
+	doc, err := goquery.NewDocumentFromReader(w.Body)
+	if err != nil {
+		t.Fatalf("failed to read response: %v", err)
+	}
+
+	if appendCount := doc.Find(`turbo-stream[action="append"][target="appendTarget"]`).Length(); appendCount != expectedAppends {
+		t.Errorf("expected %d append actions, but got %d", expectedAppends, appendCount)
+	}
+	if prependCount := doc.Find(`turbo-stream[action="prepend"][target="prependTarget"]`).Length(); prependCount != expectedPrepends {
+		t.Errorf("expected %d prepend actions, but got %d", expectedPrepends, prependCount)
+	}
+	if replaceCount := doc.Find(`turbo-stream[action="replace"][target="replaceTarget"]`).Length(); replaceCount != expectedReplaces {
+		t.Errorf("expected %d replace actions, but got %d", expectedReplaces, replaceCount)
+	}
+	if updateCount := doc.Find(`turbo-stream[action="update"][target="updateTarget"]`).Length(); updateCount != expectedUpdates {
+		t.Errorf("expected %d update actions, but got %d", expectedUpdates, updateCount)
+	}
+	if removeCount := doc.Find(`turbo-stream[action="remove"][target="removeTarget"]`).Length(); removeCount != expectedRemoves {
+		t.Errorf("expected %d remove actions, but got %d", expectedRemoves, removeCount)
+	}
+
+	if w.HeaderMap.Get("Content-Type") != "text/vnd.turbo-stream.html" {
+		t.Errorf("expected Content-Type %q, got %q", "text/vnd.turbo-stream.html", w.HeaderMap.Get("Content-Type"))
+	}
+}
+
+func TestIsTurboRequest(t *testing.T) {
+	turboRequest := httptest.NewRequest("GET", "/", nil)
+	if IsTurboRequest(turboRequest) {
+		t.Error("request was incorrectly recognised as a Turbo stream request")
+	}
+	turboRequest.Header.Add("accept", "text/vnd.turbo-stream.html")
+	if !IsTurboRequest(turboRequest) {
+		t.Error("request not correctly recognised as a Turbo stream request")
+	}
+}
