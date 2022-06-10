2f7641baa3e790e3468cd102737527a7c9d80ffe
Joe Davidson <joe.davidson.21111@gmail.com>
2022-06-10 09:15:56 +0100

    Update generator/test-templ-element/render_test.go
    Co-authored-by: Adrian Hesketh <a-h@users.noreply.github.com>

--- generator/test-templ-element/render_test.go
+++ generator/test-templ-element/render_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-const expected = `<div id="1">children1 <div id="2">children2 <div id="3">children3 <div id="4"></div></div></div></div>`
+const expected = `<div id="1">child1 <div id="2">child2 <div id="3">child3 <div id="4"></div></div></div></div>`
 
 func TestFor(t *testing.T) {
 	w := new(strings.Builder)
