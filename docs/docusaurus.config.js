b791309733903d8dd1ecad27cd914fc852f7b2e9
joerdav <joe.davidson.21111@gmail.com>
2023-04-14 11:53:53 +0100

    feat: update social-card

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/social-card.jpg',
       navbar: {
         logo: {
           alt: 'Templ Logo',
