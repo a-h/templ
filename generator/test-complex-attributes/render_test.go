c5f9a3201c6015fe1347247820cd869762b9010d
Michael Stack <michael.stack@gmail.com>
2023-05-04 15:10:44 -0600

    fix: parse @attr, :attr, and x:attr attribute names

---
+++ generator/test-complex-attributes/render_test.go
@@ -0,0 +1,23 @@
+package testcomplexattributes
+
+import (
+	_ "embed"
+	"testing"
+
+	"github.com/a-h/templ/generator/htmldiff"
+)
+
+//go:embed expected.html
+var expected string
+
+func Test(t *testing.T) {
+	component := ComplexAttributes()
+
+	diff, err := htmldiff.Diff(component, expected)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if diff != "" {
+		t.Error(diff)
+	}
+}
