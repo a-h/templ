af819625b2a957f5e0a67855297f3660d9e045f8
Adrian Hesketh <adrianhesketh@hushmail.com>
2023-08-22 17:55:29 +0100

    feat: skip walking directories with dot or underscore prefixes, or that are called vendor/node_modules to reduce time wasted walking dirs

--- cmd/templ/processor/processor.go
+++ cmd/templ/processor/processor.go
@@ -2,6 +2,7 @@ package processor
 
 import (
 	"io/fs"
+	"path"
 	"path/filepath"
 	"strings"
 	"sync"
@@ -15,13 +16,45 @@ type Result struct {
 }
 
 func Process(dir string, f func(fileName string) error, workerCount int, results chan<- Result) {
-	defer close(results)
 	templates := make(chan string)
 	go func() {
-		if err := getTemplates(dir, templates); err != nil {
+		defer close(templates)
+		if err := FindTemplates(dir, templates); err != nil {
 			results <- Result{Error: err}
 		}
 	}()
+	ProcessChannel(templates, dir, f, workerCount, results)
+}
+
+func shouldSkipDir(dir string) bool {
+	if dir == "." {
+		return false
+	}
+	if dir == "vendor" || dir == "node_modules" {
+		return true
+	}
+	_, name := path.Split(dir)
+	// These directories are ignored by the Go tool.
+	if strings.HasPrefix(name, ".") || strings.HasPrefix(name, "_") {
+		return true
+	}
+	return false
+}
+
+func FindTemplates(srcPath string, output chan<- string) (err error) {
+	return filepath.Walk(srcPath, func(currentPath string, info fs.FileInfo, err error) error {
+		if info.IsDir() && shouldSkipDir(currentPath) {
+			return filepath.SkipDir
+		}
+		if !info.IsDir() && strings.HasSuffix(currentPath, ".templ") {
+			output <- currentPath
+		}
+		return nil
+	})
+}
+
+func ProcessChannel(templates <-chan string, dir string, f func(fileName string) error, workerCount int, results chan<- Result) {
+	defer close(results)
 	var wg sync.WaitGroup
 	wg.Add(workerCount)
 	for i := 0; i < workerCount; i++ {
@@ -40,12 +73,3 @@ func Process(dir string, f func(fileName string) error, workerCount int, results
 	wg.Wait()
 }
 
-func getTemplates(srcPath string, output chan<- string) (err error) {
-	defer close(output)
-	return filepath.Walk(srcPath, func(currentPath string, info fs.FileInfo, err error) error {
-		if !info.IsDir() && strings.HasSuffix(currentPath, ".templ") {
-			output <- currentPath
-		}
-		return nil
-	})
-}
