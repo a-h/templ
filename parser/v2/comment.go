469be339164be00b57ed4a53f770b7d8cfeeeef0
Kevin Stock <kevin@toolhouse.com>
2023-09-20 11:18:25 -0700

    Simplify commentParser type
    This removes the start and end fields from the commentParser, moving them into module level variables. This simplifies the comment parser as it is unlikely that the start and end fields will need to be modified or added to in the future.

--- parser/v2/comment.go
+++ parser/v2/comment.go
@@ -6,29 +6,27 @@ import (
 	"github.com/a-h/parse"
 )
 
+var htmlCommentStart = parse.String("<!--")
+var htmlCommentEnd = parse.String("-->")
+
 type commentParser struct {
-	start parse.Parser[string]
-	end   parse.Parser[string]
 }
 
-var htmlComment = commentParser{
-	start: parse.String("<!--"),
-	end:   parse.String("-->"),
-}
+var htmlComment = commentParser{}
 
 func (p commentParser) Parse(pi *parse.Input) (c Comment, ok bool, err error) {
 	// Comment start.
-	if _, ok, err = p.start.Parse(pi); err != nil || !ok {
+	if _, ok, err = htmlCommentStart.Parse(pi); err != nil || !ok {
 		return
 	}
 
 	// Once we've got the comment start sequence, parse anything until the end
 	// sequence as the comment contents.
-	if c.Contents, ok, err = Must(parse.StringUntil(p.end), fmt.Sprintf("expected end comment sequence not found")).Parse(pi); err != nil || !ok {
+	if c.Contents, ok, err = Must(parse.StringUntil(htmlCommentEnd), fmt.Sprintf("expected end comment sequence not found")).Parse(pi); err != nil || !ok {
 		return
 	}
 	// Cut the end element.
-	_, _, _ = p.end.Parse(pi)
+	_, _, _ = htmlCommentEnd.Parse(pi)
 
 	return c, true, nil
 }
