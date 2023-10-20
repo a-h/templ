33d4af4670fcf4b0736aca2d8b737f802be62e88
BKR-dev <giga@sigmachad.quest>
2023-10-20 14:46:15 +0200

    adds more standart html tags in snippets

--- cmd/templ/lspcmd/proxy/snippets.go
+++ cmd/templ/lspcmd/proxy/snippets.go
@@ -18,6 +18,12 @@ var htmlSnippets = []lsp.CompletionItem{
 		InsertTextFormat: lsp.InsertTextFormatSnippet,
 	},
 	{
+		Label:            "button",
+		InsertText:       `button type="button" ${1:}>${2:}</button>`,
+		Kind:             lsp.CompletionItemKind(lsp.CompletionItemKindSnippet),
+		InsertTextFormat: lsp.InsertTextFormatSnippet,
+	},
+	{
 		Label: "div",
 		InsertText: `div>
 	${0}
@@ -25,4 +31,71 @@ var htmlSnippets = []lsp.CompletionItem{
 		Kind:             lsp.CompletionItemKind(lsp.CompletionItemKindSnippet),
 		InsertTextFormat: lsp.InsertTextFormatSnippet,
 	},
+	{
+		Label: "p",
+		InsertText: `p>
+    ${0}
+</p>`,
+		Kind:             lsp.CompletionItemKind(lsp.CompletionItemKindSnippet),
+		InsertTextFormat: lsp.InsertTextFormatSnippet,
+	},
+	{
+		Label: "head",
+		InsertText: `head>
+    ${0}
+</head>`,
+		Kind:             lsp.CompletionItemKind(lsp.CompletionItemKindSnippet),
+		InsertTextFormat: lsp.InsertTextFormatSnippet,
+	},
+	{
+		Label: "body",
+		InsertText: `body>
+    ${0}
+</body>`,
+		Kind:             lsp.CompletionItemKind(lsp.CompletionItemKindSnippet),
+		InsertTextFormat: lsp.InsertTextFormatSnippet,
+	},
+	{
+		Label:            "title",
+		InsertText:       `title>${0}</title>`,
+		Kind:             lsp.CompletionItemKind(lsp.CompletionItemKindSnippet),
+		InsertTextFormat: lsp.InsertTextFormatSnippet,
+	},
+	{
+		Label:            "h1",
+		InsertText:       `h1>${0}</h1>`,
+		Kind:             lsp.CompletionItemKind(lsp.CompletionItemKindSnippet),
+		InsertTextFormat: lsp.InsertTextFormatSnippet,
+	},
+	{
+		Label:            "h2",
+		InsertText:       `h2>${0}</h2>`,
+		Kind:             lsp.CompletionItemKind(lsp.CompletionItemKindSnippet),
+		InsertTextFormat: lsp.InsertTextFormatSnippet,
+	},
+	{
+		Label:            "h3",
+		InsertText:       `h3>${0}</h3>`,
+		Kind:             lsp.CompletionItemKind(lsp.CompletionItemKindSnippet),
+		InsertTextFormat: lsp.InsertTextFormatSnippet,
+	},
+	{
+		Label:            "h4",
+		InsertText:       `h4>${0}</h4>`,
+		Kind:             lsp.CompletionItemKind(lsp.CompletionItemKindSnippet),
+		InsertTextFormat: lsp.InsertTextFormatSnippet,
+	},
+	{
+		Label:            "h5",
+		InsertText:       `h5>${0}</h5>`,
+		Kind:             lsp.CompletionItemKind(lsp.CompletionItemKindSnippet),
+		InsertTextFormat: lsp.InsertTextFormatSnippet,
+	},
+	{
+		Label:            "h6",
+		InsertText:       `h6>${0}</h6>`,
+		Kind:             lsp.CompletionItemKind(lsp.CompletionItemKindSnippet),
+		InsertTextFormat: lsp.InsertTextFormatSnippet,
+	},
 }
+
