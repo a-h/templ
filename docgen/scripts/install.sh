6277de2f1163f198c29d550baad63a8cb5e0d53c
Jonas Plum <git@cugu.eu>
2024-01-07 01:01:47 +0100

    fix: footer colors

--- docgen/scripts/install.sh
+++ docgen/scripts/install.sh
@@ -1,5 +1,1 @@
 npm install tailwindcss@latest @tailwindcss/typography
-npm install -g postcss@latest autoprefixer@latest
-npm install -D postcss@latest autoprefixer@latest
-npm install -g live-server@latest
-npm install -D live-server@latest
