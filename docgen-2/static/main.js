23c0d07067ec1628204f95d01787c40544527b7f
nate <16102404+nathan-hello@users.noreply.github.com>
2024-02-18 09:28:24 +0000

    added static folder

---
+++ docgen-2/static/main.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menu_toggles = document.querySelectorAll('.menu-toggle');
+    menu_toggles.forEach(menu_toggle => {
+        menu_toggle.addEventListener('click', () => {
+            toggleSidebar();
+        });
+    });
+
+    const overlay = document.getElementById('overlay');
+    if (overlay !== null) {
+        overlay.addEventListener('click', () => {
+            if (overlay.classList.contains('opacity-70')) {
+                toggleSidebar();
+            }
+        });
+    }
+
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark')
+
+        document.getElementById('theme').href = document.getElementById('theme').href.replace('light', 'dark');
+    } else {
+        document.documentElement.classList.remove('dark')
+
+        document.getElementById('theme').href = document.getElementById('theme').href.replace('dark', 'light');
+    }
+
+    const dark_mode_toggles = document.querySelectorAll('.dark-mode-toggle');
+    dark_mode_toggles.forEach(dark_mode_toggle => {
+        dark_mode_toggle.addEventListener('click', () => {
+            let theme = localStorage.theme;
+            if (theme === 'dark') {
+                localStorage.theme = 'light';
+            } else {
+                localStorage.theme = 'dark';
+            }
+            document.documentElement.classList.toggle('dark');
+
+            let theme_link = document.getElementById('theme');
+            theme_link.href = theme_link.href.replace(theme, localStorage.theme);
+        });
+    });
+});
+
+function toggleSidebar() {
+    let menu = document.getElementById('sidebar');
+    menu.classList.toggle('open');
+
+    toggleOverlay();
+
+    let body = document.getElementsByTagName('body')[0];
+    body.classList.toggle('overflow-hidden');
+}
+
+function toggleOverlay() {
+    let sidebarOverlay = document.getElementById('overlay');
+    sidebarOverlay.classList.toggle('opacity-0');
+    sidebarOverlay.classList.toggle('opacity-70');
+    setTimeout(() => {
+        sidebarOverlay.classList.toggle('hidden');
+    }, 200);
+}
