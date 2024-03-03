f2ce5e3db2213b302be22553001de71550dc0152
Adrian Hesketh <a-h@users.noreply.github.com>
2024-03-03 15:49:54 +0000

    chore: add gzip test for proxy feature (#582)
    Co-authored-by: JamesTiberusKirk <dumitru.v.dv@gmail.com>

--- cmd/templ/generatecmd/proxy/proxy.go
+++ cmd/templ/generatecmd/proxy/proxy.go
@@ -32,6 +32,61 @@ type Handler struct {
 	sse    *sse.Handler
 }
 
+func updateGzipResponse(r *http.Response) error {
+	plainr, err := gzip.NewReader(r.Body)
+	if err != nil {
+		return err
+	}
+	defer plainr.Close()
+	body, err := io.ReadAll(plainr)
+	if err != nil {
+		return err
+	}
+	updated := insertScriptTagIntoBody(string(body))
+	var buf bytes.Buffer
+	gzw := gzip.NewWriter(&buf)
+	defer gzw.Close()
+	_, err = gzw.Write([]byte(updated))
+	if err != nil {
+		return err
+	}
+	err = gzw.Close()
+	if err != nil {
+		return err
+	}
+	r.Body = io.NopCloser(&buf)
+	r.ContentLength = int64(buf.Len())
+	r.Header.Set("Content-Length", strconv.Itoa(buf.Len()))
+	return nil
+}
+
+func updatePlainResponse(r *http.Response) error {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+	updated := insertScriptTagIntoBody(string(body))
+	r.Body = io.NopCloser(strings.NewReader(updated))
+	r.ContentLength = int64(len(updated))
+	r.Header.Set("Content-Length", strconv.Itoa(len(updated)))
+	return nil
+}
+
+func insertScriptTagIntoBody(body string) (updated string) {
+	return strings.Replace(body, "</body>", scriptTag+"</body>", -1)
+}
+
+func modifyResponse(r *http.Response) error {
+	if contentType := r.Header.Get("Content-Type"); !strings.HasPrefix(contentType, "text/html") {
+		return nil
+	}
+	modifier := updatePlainResponse
+	if r.Header.Get("Content-Encoding") == "gzip" {
+		modifier = updateGzipResponse
+	}
+	return modifier(r)
+}
+
 func New(port int, target *url.URL) *Handler {
 	p := httputil.NewSingleHostReverseProxy(target)
 	p.ErrorLog = log.New(os.Stderr, "Proxy to target error: ", 0)
@@ -40,58 +95,7 @@ func New(port int, target *url.URL) *Handler {
 		initialDelay:    100 * time.Millisecond,
 		backoffExponent: 1.5,
 	}
-	p.ModifyResponse = func(r *http.Response) error {
-		if contentType := r.Header.Get("Content-Type"); !strings.HasPrefix(contentType, "text/html") {
-			return nil
-		}
-
-		var reader io.ReadCloser
-		encoding := r.Header.Get("Content-Encoding")
-		switch encoding {
-		case "gzip":
-			plainr, err := gzip.NewReader(r.Body)
-			if err != nil {
-				return err
-			}
-			defer plainr.Close()
-			reader = plainr
-		default:
-			reader = r.Body
-		}
-
-		body, err := io.ReadAll(reader)
-		if err != nil {
-			return err
-		}
-
-		updated := strings.Replace(string(body), "</body>", scriptTag+"</body>", -1)
-
-		switch encoding {
-		case "gzip":
-			var buf bytes.Buffer
-			gzw := gzip.NewWriter(&buf)
-			defer gzw.Close()
-
-			_, err = gzw.Write([]byte(updated))
-			if err != nil {
-				return err
-			}
-			err = gzw.Close()
-			if err != nil {
-				return err
-			}
-
-			r.Body = io.NopCloser(&buf)
-			r.ContentLength = int64(buf.Len())
-			r.Header.Set("Content-Length", strconv.Itoa(buf.Len()))
-		default:
-			r.Body = io.NopCloser(strings.NewReader(updated))
-			r.ContentLength = int64(len(updated))
-			r.Header.Set("Content-Length", strconv.Itoa(len(updated)))
-		}
-
-		return nil
-	}
+	p.ModifyResponse = modifyResponse
 	return &Handler{
 		URL:    fmt.Sprintf("http://127.0.0.1:%d", port),
 		Target: target,
---
+++ cmd/templ/generatecmd/proxy/proxy_test.go
@@ -0,0 +1,152 @@
+package proxy
+
+import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestProxy(t *testing.T) {
+	t.Run("plain: non-html content is not modified", func(t *testing.T) {
+		// Arrange
+		r := &http.Response{
+			Body:   io.NopCloser(strings.NewReader(`{"key": "value"}`)),
+			Header: make(http.Header),
+		}
+		r.Header.Set("Content-Type", "application/json")
+		r.Header.Set("Content-Length", "16")
+
+		// Act
+		err := modifyResponse(r)
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		// Assert
+		if r.Header.Get("Content-Length") != "16" {
+			t.Errorf("expected content length to be 16, got %v", r.Header.Get("Content-Length"))
+		}
+		actualBody, err := io.ReadAll(r.Body)
+		if err != nil {
+			t.Fatalf("unexpected error reading response: %v", err)
+		}
+		if diff := cmp.Diff(`{"key": "value"}`, string(actualBody)); diff != "" {
+			t.Errorf("unexpected response body (-got +want):\n%s", diff)
+		}
+	})
+	t.Run("plain: body tags get the script inserted", func(t *testing.T) {
+		// Arrange
+		r := &http.Response{
+			Body:   io.NopCloser(strings.NewReader(`<html><body></body></html>`)),
+			Header: make(http.Header),
+		}
+		r.Header.Set("Content-Type", "text/html, charset=utf-8")
+		r.Header.Set("Content-Length", "26")
+
+		expectedString := insertScriptTagIntoBody(`<html><body></body></html>`)
+		if !strings.Contains(expectedString, scriptTag) {
+			t.Fatalf("expected the script tag to be inserted, but it wasn't: %q", expectedString)
+		}
+
+		// Act
+		err := modifyResponse(r)
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		// Assert
+		if r.Header.Get("Content-Length") != fmt.Sprintf("%d", len(expectedString)) {
+			t.Errorf("expected content length to be %d, got %v", len(expectedString), r.Header.Get("Content-Length"))
+		}
+		actualBody, err := io.ReadAll(r.Body)
+		if err != nil {
+			t.Fatalf("unexpected error reading response: %v", err)
+		}
+		if diff := cmp.Diff(expectedString, string(actualBody)); diff != "" {
+			t.Errorf("unexpected response body (-got +want):\n%s", diff)
+		}
+	})
+	t.Run("gzip: non-html content is not modified", func(t *testing.T) {
+		// Arrange
+		r := &http.Response{
+			Body:   io.NopCloser(strings.NewReader(`{"key": "value"}`)),
+			Header: make(http.Header),
+		}
+		r.Header.Set("Content-Type", "application/json")
+		// It's not actually gzipped here, but it doesn't matter, it shouldn't get that far.
+		r.Header.Set("Content-Encoding", "gzip")
+		// Similarly, this is not the actual length of the gzipped content.
+		r.Header.Set("Content-Length", "16")
+
+		// Act
+		err := modifyResponse(r)
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		// Assert
+		if r.Header.Get("Content-Length") != "16" {
+			t.Errorf("expected content length to be 16, got %v", r.Header.Get("Content-Length"))
+		}
+		actualBody, err := io.ReadAll(r.Body)
+		if err != nil {
+			t.Fatalf("unexpected error reading response: %v", err)
+		}
+		if diff := cmp.Diff(`{"key": "value"}`, string(actualBody)); diff != "" {
+			t.Errorf("unexpected response body (-got +want):\n%s", diff)
+		}
+	})
+	t.Run("gzip: body tags get the script inserted", func(t *testing.T) {
+		// Arrange
+		body := `<html><body></body></html>`
+		var buf bytes.Buffer
+		gzw := gzip.NewWriter(&buf)
+		gzw.Write([]byte(body))
+		gzw.Close()
+
+		expectedString := insertScriptTagIntoBody(body)
+
+		var expectedBytes bytes.Buffer
+		gzw = gzip.NewWriter(&expectedBytes)
+		gzw.Write([]byte(expectedString))
+		gzw.Close()
+		expectedLength := len(expectedBytes.Bytes())
+
+		r := &http.Response{
+			Body:   io.NopCloser(&buf),
+			Header: make(http.Header),
+		}
+		r.Header.Set("Content-Type", "text/html, charset=utf-8")
+		r.Header.Set("Content-Encoding", "gzip")
+		r.Header.Set("Content-Length", fmt.Sprintf("%d", expectedLength))
+
+		// Act
+		err := modifyResponse(r)
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		// Assert
+		if r.Header.Get("Content-Length") != fmt.Sprintf("%d", expectedLength) {
+			t.Errorf("expected content length to be %d, got %v", expectedLength, r.Header.Get("Content-Length"))
+		}
+
+		gr, err := gzip.NewReader(r.Body)
+		if err != nil {
+			t.Fatalf("unexpected error reading response: %v", err)
+		}
+		actualBody, err := io.ReadAll(gr)
+		if err != nil {
+			t.Fatalf("unexpected error reading response: %v", err)
+		}
+		if diff := cmp.Diff(expectedString, string(actualBody)); diff != "" {
+			t.Errorf("unexpected response body (-got +want):\n%s", diff)
+		}
+	})
+}
