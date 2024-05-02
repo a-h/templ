9281711b0fd4eba9e088366911f43f4c25418ed3
Robert Kolmos <robertkolmos@gmail.com>
2024-05-02 13:00:50 -0700

    change test name to be better aligned with other attribute tests

--- generator/test-attribute-regex/template_templ.go
+++ generator/test-attribute-regex/template_templ.go
@@ -9,7 +9,7 @@ import "context"
 import "io"
 import "bytes"
 
-func render() templ.Component {
+func BasicTemplate() templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
 		if !templ_7745c5c3_IsBuffer {
