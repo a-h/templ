702c106354a48e299d72ea6bde0e1527b0311d3f
Jackie Li <jackie.space@gmail.com>
2024-05-03 10:40:30 +0100

    fix: allow hot reload script to be re-evaluated, fixes #716

--- cmd/templ/generatecmd/proxy/script.js
+++ cmd/templ/generatecmd/proxy/script.js
@@ -1,6 +1,9 @@
-let templ_reloadSrc = new EventSource("/_templ/reload/events");
-templ_reloadSrc.onmessage = (event) => {
-	if (event && event.data === "reload") {
-		window.location.reload();
-	}
-};
+(function () {
+  let templ_reloadSrc = window.templ_reloadSrc || new EventSource("/_templ/reload/events");
+  templ_reloadSrc.onmessage = (event) => {
+    if (event && event.data === "reload") {
+      window.location.reload();
+    }
+  };
+  window.templ_reloadSrc = templ_reloadSrc;
+})();
