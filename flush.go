b7645fde8f2f138ec9b64edb59214d7542ae94a8
Adrian Hesketh <adrianhesketh@hushmail.com>
2024-06-22 15:15:26 +0100

    chore: add tests for flush component

--- flush.go
+++ flush.go
@@ -6,6 +6,7 @@ import (
 	"io"
 )
 
+// Flush flushes the output buffer after all its child components have been rendered.
 func Flush() FlushComponent {
 	return FlushComponent{}
 }
@@ -13,17 +14,24 @@ func Flush() FlushComponent {
 type FlushComponent struct {
 }
 
-type flusher interface {
+type flusherError interface {
 	Flush() error
+}
+
+type flusher interface {
+	Flush()
 }
 
 func (f FlushComponent) Render(ctx context.Context, w io.Writer) (err error) {
 	if err = GetChildren(ctx).Render(ctx, w); err != nil {
 		return err
 	}
-	b, isTemplBuffer := w.(flusher)
-	if !isTemplBuffer {
-		return fmt.Errorf("unable to flush, writer is not flushable")
+	switch w := w.(type) {
+	case flusher:
+		w.Flush()
+		return nil
+	case flusherError:
+		return w.Flush()
 	}
-	return b.Flush()
+	return fmt.Errorf("unable to flush, writer is not flushable")
 }
