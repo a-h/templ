23d46844d782cdb69c9c49ec44eddc7ea94740bc
nathan-hello <natey3k@gmail.com>
2024-07-05 18:47:33 -0400

    tailwind config format with prettier

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,48 +1,36 @@
 const disabledCss = {
-    'code::before': false,
-    'code::after': false,
-    'blockquote p:first-of-type::before': false,
-    'blockquote p:last-of-type::after': false,
-    pre: false,
-    code: false,
-    'pre code': false,
-    'code::before': false,
-    'code::after': false,
+  "code::before": false,
+  "code::after": false,
+  "blockquote p:first-of-type::before": false,
+  "blockquote p:last-of-type::after": false,
+  pre: false,
+  code: false,
+  "pre code": false,
+  "code::before": false,
+  "code::after": false,
 };
 
 const defaultCss = {
-    ...disabledCss,
-    maxWidth: '96ch',
+  ...disabledCss,
+  maxWidth: "96ch",
 };
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: 'class',
-    content: [
-        "./static/*.js",
-        "./src/components/*.templ"
-    ],
-    safelist: [
-        "anchor",
-        "note",
-        "tip",
-        "info",
-        "warning",
-        "critical",
-        "caution"
-    ],
-    theme: {
-        extend: {
-            typography: {
-                DEFAULT: { css: defaultCss },
-                sm: { css: disabledCss },
-                lg: { css: disabledCss },
-                xl: { css: disabledCss },
-                '2xl': { css: disabledCss },
-            },
-        },
+  darkMode: "class",
+  content: ["./static/*.js", "./src/components/*.templ"],
+  safelist: ["anchor", "note", "tip", "info", "warning", "critical", "caution"],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: { css: defaultCss },
+        sm: { css: disabledCss },
+        lg: { css: disabledCss },
+        xl: { css: disabledCss },
+        "2xl": { css: disabledCss },
+      },
+      colors: {},
     },
-    plugins: [
-        require('@tailwindcss/typography'),
-    ],
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
