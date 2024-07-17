3d6035f71e795749728d291db6c6859946c6b9d9
Corné de Jong <5366568-cornedejong@users.noreply.gitlab.com>
2024-07-17 18:10:38 +0200

    better comments, refactored the test

--- js_expression.go
+++ js_expression.go
@@ -1,12 +1,15 @@
 package templ
 
-// JsVar refers to a JS variable name.
-// The string value of JsVar will be used as the variable argument in the function call
+// JsExpression represents a JavaScript expression intended for use as an argument for script blocks.
+// The string value of JsExpression will be inserted directly as JavaScript code in function call arguments.
 type JsExpression string
 
 const (
-	// JsEvent represents the "event" variable in Javascript
+	// JsEvent corresponds to the "event" variable in JavaScript, typically representing the event object
+	// that is passed to event handler functions.
 	JsEvent JsExpression = "event"
-	// JsTargetElement represents the "this" variable in Javascript
+
+	// JsTargetElement corresponds to the "this" keyword in JavaScript, usually representing the element
+	// that the event handler is bound to.
 	JsTargetElement JsExpression = "this"
 )
--- js_expression_test.go
+++ js_expression_test.go
@@ -4,19 +4,11 @@ import (
 	"testing"
 )
 
-func TestJsVar(t *testing.T) {
-	functionName := "myJsFunction"
-	params := []any{
-		"StringValue",
-		123,
-		JsExpression("event"),
-		JsExpression("1 + 2"),
-	}
-
+func TestJsExpression(t *testing.T) {
 	expected := "myJsFunction(\"StringValue\",123,event,1 + 2)"
-	result := SafeScriptInline(functionName, params...)
+	result := SafeScriptInline("myJsFunction", "StringValue", 123, JsExpression("event"), JsExpression("1 + 2"))
 
 	if result != expected {
-		t.Fatalf("Expected '%s' but got '%s'", expected, result)
+		t.Fatalf("TestJsExpression: Expected '%s' but got '%s'", expected, result)
 	}
 }
