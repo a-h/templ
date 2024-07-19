45b62824f521df35148f16ac8ef23412eedd8911
Corné de Jong <5366568-cornedejong@users.noreply.gitlab.com>
2024-07-19 09:53:19 +0200

    removed constants

--- js_expression.go
+++ js_expression.go
@@ -1,15 +1,5 @@
 package templ
 
-// JsExpression represents a JavaScript expression intended for use as an argument for script blocks.
+// JsExpression represents a JavaScript expression intended for use as an argument for script templates.
 // The string value of JsExpression will be inserted directly as JavaScript code in function call arguments.
 type JsExpression string
-
-const (
-	// JsEvent corresponds to the "event" variable in JavaScript, typically representing the event object
-	// that is passed to event handler functions.
-	JsEvent JsExpression = "event"
-
-	// JsTargetElement corresponds to the "this" keyword in JavaScript, usually representing the element
-	// that the event handler is bound to.
-	JsTargetElement JsExpression = "this"
-)
