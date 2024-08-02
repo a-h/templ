45ef62745cacdb74825b48d933a01a2c2ea55955
Corné de Jong <me@corne.co>
2024-08-02 21:01:07 +0200

    fix rename in test
    Co-authored-by: Adrian Hesketh <a-h@users.noreply.github.com>

--- js_expression_test.go
+++ js_expression_test.go
@@ -4,11 +4,11 @@ import (
 	"testing"
 )
 
-func TestJsExpression(t *testing.T) {
-	expected := "myJsFunction(\"StringValue\",123,event,1 + 2)"
-	result := SafeScriptInline("myJsFunction", "StringValue", 123, JsExpression("event"), JsExpression("1 + 2"))
+func TestJSExpression(t *testing.T) {
+	expected := "myJSFunction(\"StringValue\",123,event,1 + 2)"
+	actual := SafeScriptInline("myJSFunction", "StringValue", 123, JSExpression("event"), JSExpression("1 + 2"))
 
-	if result != expected {
-		t.Fatalf("TestJsExpression: Expected '%s' but got '%s'", expected, result)
+	if actual != expected {
+		t.Fatalf("TestJSExpression: expected %q, got %q", expected, actual)
 	}
 }
