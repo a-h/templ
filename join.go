b6f623fb52400ff7918b1bce69155c54070997ef
Arman <arman1apollon@gmail.com>
2024-09-28 21:19:16 +0100

    docs: updated join function comment

--- join.go
+++ join.go
@@ -5,7 +5,8 @@ import (
 	"io"
 )
 
-// Pass any number of templ.Components to get a single templ.Component with the components rendered
+// Join returns a single `templ.Component` that will render provided components in order.
+// If any of the components return an error the Join component will immediately return with the error.
 func Join(components ...Component) Component {
 	return ComponentFunc(func(ctx context.Context, w io.Writer) (err error) {
 		for _, c := range components {
