c32b7116de0e0e6221305a66681fd7d6219ff3b9
Adrian Hesketh <inbox@adrianhesketh.com>
2025-08-05 17:27:57 +0100

    chore: fix linter warning

--- cmd/templ/generatecmd/proxy/proxy.go
+++ cmd/templ/generatecmd/proxy/proxy.go
@@ -243,7 +243,7 @@ type roundTripper struct {
 func (rt *roundTripper) setShouldSkipResponseModificationHeader(r *http.Request, resp *http.Response) {
 	// Instruct the modifyResponse function to skip modifying the response if the
 	// HTTP request has come from HTMX or Datastar.
-	if !(r.Header.Get("HX-Request") == "true" || r.Header.Get("Datastar-Request") == "true") {
+	if r.Header.Get("HX-Request") != "true" && r.Header.Get("Datastar-Request") != "true" {
 		return
 	}
 	resp.Header.Set("templ-skip-modify", "true")
